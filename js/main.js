22ec78bc9273a4973eba85df162d4cc985b48852
Steven Peralta <peralta.steven01@gmail.com>
2026-06-19 10:37:13 -0700

    Added skils to about section and caption in side panel. Also added favicon for page.

--- js/main.js
+++ js/main.js
@@ -77,6 +77,9 @@ function generateNR() {
     const title = document.createElement('h2');
     title.classList.add('side-title');
     title.textContent = 'Nuevo Remanente Website';
+    const titleCaption = document.createElement('p');
+    titleCaption.classList.add('side-title-caption');
+    titleCaption.textContent = 'HTML • CSS • JavaScript • EmailJS';
     const nrDescription = document.createElement('p');
     nrDescription.classList.add('description');
     nrDescription.textContent = 'This website was made for a local church in order to have place online where new visitors can learn more about their church. The tech stack that was used includes HTML5, CSS3, JavaScript, and EmailJS. Site includes an animated menu where users can navigate to other pages. Pages inlude: About Page, Service Information Page, and a contact page where users can send requests via EmailJS.';
@@ -87,20 +90,23 @@ function generateNR() {
       e.preventDefault();
       window.open('https://www.nuevoremanente.com/index.html', '_blank');
     });
-    sideContentContainer.append(title, nrDescription, nrBtn);
+    const nrGit = document.createElement('button');
+    nrGit.classList.add('git-btn');
+    nrGit.textContent = 'View Code';
+    nrGit.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.open('https://github.com/Pianist01/nr-overhaul', '_blank');
+    });
+    sideContentContainer.append(title, titleCaption, nrDescription, nrBtn, nrGit);
     projects.append(sideContentContainer);
 
     requestAnimationFrame(() => {
       sideContentContainer.classList.add('active');
-    });
-    requestAnimationFrame(() => {
       title.classList.add('active');
-    });
-    requestAnimationFrame(() => {
+      titleCaption.classList.add('active');
       nrDescription.classList.add('active');
-    });
-    requestAnimationFrame(() => {
       nrBtn.classList.add('active');
+      nrGit.classList.add('active');
     });
 
     exit.addEventListener('click', (e) => {
@@ -108,8 +114,10 @@ function generateNR() {
       requestAnimationFrame(() => {
         sideContentContainer.classList.remove('active');
         title.classList.remove('active');
+        titleCaption.classList.remove('active');
         nrDescription.classList.remove('active');
         nrBtn.classList.remove('active');
+        nrGit.classList.remove('active');
         title.textContent = '';
         nrDescription.textContent = '';
       });
@@ -123,6 +131,9 @@ function generateWeightTracker() {
   const title = document.createElement('h2');
   title.classList.add('side-title');
   title.textContent = 'Meal Plan';
+  const titleCaption = document.createElement('p');
+  titleCaption.classList.add('side-title-caption');
+  titleCaption.textContent = 'HTML • CSS • JavaScript';
   const mealPlanDescription = document.createElement('p');
   mealPlanDescription.classList.add('description');
   mealPlanDescription.textContent = 'This project is meant to be a simple version of a calorie calculator. Users are asked a series of questions: name, age, height, weight, etc. Once information is submitted, a new page is created organizing their data. The calories needed to achieve their goal is displayed along with some food recommendations. This project relied heavily on JavaScript to generate the majority of the content displayed, but HTML5 and CSS3 were still used.';
@@ -132,20 +143,22 @@ function generateWeightTracker() {
   mealBtn.addEventListener('click', (e) => {
     window.open('https://pianist01.github.io/meal-plan/', '_blank');
   });
-  sideContentContainer.append(title, mealPlanDescription, mealBtn);
+  const mealGit = document.createElement('button');
+  mealGit.classList.add('git-btn');
+  mealGit.textContent = 'View Code';
+  mealGit.addEventListener('click', (e) => {
+    window.open('https://github.com/Pianist01/meal-plan', '_blank');
+  });
+  sideContentContainer.append(title, titleCaption, mealPlanDescription, mealBtn, mealGit);
   projects.append(sideContentContainer);
 
   requestAnimationFrame(() => {
     sideContentContainer.classList.add('active');
-  });
-  requestAnimationFrame(() => {
     title.classList.add('active');
-  });
-  requestAnimationFrame(() => {
+    titleCaption.classList.add('active');
     mealPlanDescription.classList.add('active');
-  });
-  requestAnimationFrame(() => {
     mealBtn.classList.add('active');
+    mealGit.classList.add('active');
   });
 
   exit.addEventListener('click', (e) => {
@@ -153,8 +166,10 @@ function generateWeightTracker() {
     requestAnimationFrame(() => {
       sideContentContainer.classList.remove('active');
       title.classList.remove('active');
+      titleCaption.classList.remove('active');
       mealPlanDescription.classList.remove('active');
       mealBtn.classList.remove('active');
+      mealGit.classList.remove('active');
       title.textContent = '';
       mealPlanDescription.textContent = '';
     });
@@ -168,6 +183,9 @@ function generateKeys() {
   const title = document.createElement('h2');
   title.classList.add('side-title');
   title.textContent = 'The Keys';
+  const titleCaption = document.createElement('p');
+  titleCaption.classList.add('side-title-caption');
+  titleCaption.textContent = 'HTML • CSS • JavaScript';
   const keysDescription = document.createElement('p');
   keysDescription.classList.add('description');
   keysDescription.textContent = 'This project is an example landing page for an E-commerce business that sells keyboards with 3 sections. The first section of the landing page includes a hero container with brief description of product along with CTA button. The second section allows user to read why product is great, and final section allows users to read a few reviews to further convince them. At the bottom of the page is another CTA button.';
@@ -177,20 +195,22 @@ function generateKeys() {
   keysBtn.addEventListener('click', (e) => {
     window.open('https://pianist01.github.io/keyboard-page/', '_blank');
   });
-  sideContentContainer.append(title, keysDescription, keysBtn);
+  const keyGit = document.createElement('button');
+  keyGit.classList.add('git-btn');
+  keyGit.textContent = 'View Code';
+  keyGit.addEventListener('click', (e) => {
+    window.open('https://github.com/Pianist01/keyboard-page', '_blank');
+  });
+  sideContentContainer.append(title, titleCaption, keysDescription, keysBtn, keyGit);
   projects.append(sideContentContainer);
 
   requestAnimationFrame(() => {
     sideContentContainer.classList.add('active');
-  });
-  requestAnimationFrame(() => {
     title.classList.add('active');
-  });
-  requestAnimationFrame(() => {
+    titleCaption.classList.add('active');
     keysDescription.classList.add('active');
-  });
-  requestAnimationFrame(() => {
     keysBtn.classList.add('active');
+    keyGit.classList.add('active');
   });
 
   exit.addEventListener('click', (e) => {
@@ -198,8 +218,10 @@ function generateKeys() {
     requestAnimationFrame(() => {
       sideContentContainer.classList.remove('active');
       title.classList.remove('active');
+      titleCaption.classList.remove('active');
       keysDescription.classList.remove('active');
       keysBtn.classList.remove('active');
+      keyGit.classList.remove('active');
       title.textContent = '';
       keysDescription.textContent = '';
     });
